25aab3402f0aaabb390c68d5d0522a0ce386f8f3
Adonis2030 <service.provider2030@gmail.com>
2024-05-19 18:40:22 +0200

    seperate components

---
+++ src/components/CatsRock.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="rock-container">
+    <div>Cats Rock</div>
+  </div>
+</template>
+
+<script lang="ts">
+// import { Options, Vue } from "vue-class-component";
+
+// @Options({
+//   props: {
+//     msg: String,
+//   },
+// })
+// export default class CatsRock extends Vue {
+//   // msg!: string;
+// }
+export default {
+  name: "CatsRock",
+};
+</script>
---
+++ src/views/RockView.vue
@@ -0,0 +1,25 @@
+<template>
+  <cats-rock />
+</template>
+
+<style lang="scss">
+body {
+  height: 100vh;
+  background-image: url("@/assets/back.jpeg");
+  background-repeat: no-repeat;
+  background-size: 100% 100%;
+  margin: 0;
+}
+</style>
+
+<script lang="ts">
+import { Options, Vue } from "vue-class-component";
+import CatsRock from "@/components/CatsRock.vue";
+
+@Options({
+  components: {
+    "cats-rock": CatsRock,
+  },
+})
+export default class RockView extends Vue {}
+</script>
